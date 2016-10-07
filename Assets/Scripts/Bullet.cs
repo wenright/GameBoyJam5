1c385c0ba1558e600a48bc13959f1a17237139bf
wenright <wenright@purdue.edu>
2016-10-06 22:28:51 -0400

    Initial game ideas

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public GameObject explosion;
+
+	private int speed = 30000;
+	private int lifetime = 2;
+
+	private Vector3 lastPosition;
+
+	void Start () {
+		lastPosition = transform.position;
+		GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+		Destroy(gameObject, lifetime);
+	}
+
+	void FixedUpdate () {
+		RaycastHit hit;
+
+		if (Physics.Linecast(lastPosition, transform.position, out hit)) {
+			Destroy(Instantiate(explosion, lastPosition, transform.rotation), 2);
+			// TODO do damage here
+		}
+
+		lastPosition = transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject bullet;
+
+	private int orbitalRadius = 200;
+	private float orbitalSpeed = 0.1f;
+	private float currentOrbitalTime = 0.0f;
+	private int height = 500;
+
+	private float lookSpeed = 100.0f;
+
+	private Vector3 lookPosition = Vector3.zero;
+	private Vector3 lookOffset = Vector3.zero;
+	
+	void Update () {
+		currentOrbitalTime += Time.deltaTime * orbitalSpeed;
+		transform.position = new Vector3(Mathf.Sin(currentOrbitalTime) * orbitalRadius, height, Mathf.Cos(currentOrbitalTime) * orbitalRadius);
+
+		transform.LookAt(lookPosition);
+
+		lookOffset.x += Input.GetAxis("Mouse X") * lookSpeed * Time.deltaTime;
+		lookOffset.y -= Input.GetAxis("Mouse Y") * lookSpeed * Time.deltaTime;
+
+		transform.Rotate(lookOffset.y, lookOffset.x, 0);
+
+		if (Input.GetButtonDown("Fire1")) {
+			Instantiate(bullet, transform.position + new Vector3(0, -5, 0), transform.rotation);
+		}
+	}
+
+	private Vector3 GetLookPosition () {
+		RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit)) {
+            return hit.point;
+        }
+
+        return Vector3.zero;
+	}
+}
